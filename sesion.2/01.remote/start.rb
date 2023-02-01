600a3d307aec10f0a795e0035c9931f1c9b7c42d
dvarrui <dvarrui@protonmail.com>
2023-02-01 15:16:30 +0000

    N

---
+++ sesion.2/01.remote/start.rb
@@ -0,0 +1,15 @@
+group "GROUP NAME" do
+  target "Crear el usuario david"
+  run "getent passwd david", on: :debian
+  expect "david:"
+
+  target "Crear el usuario sergio"
+  run "cat /etc/passwd", on: :debian
+  expect "sergio:"
+end
+
+play do
+  show
+  export 
+  send copy_to: :debian
+end
