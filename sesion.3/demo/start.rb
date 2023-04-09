78045b4da37fcc8d334924d967aa0d6bb0fa9446
dvarrui <dvarrui@protonmail.com>
2023-04-09 13:23:58 +0100

    N

---
+++ sesion.3/demo/start.rb
@@ -0,0 +1,16 @@
+group "Demo" do
+  target "Crear el fichero leeme.txt en home/vagrant"
+  run "vdir /home/vagrant", on: :host
+  expect_one [ "-rw-r--r--", "leeme.txt" ]
+
+  target "Escribir IDP2223 dentro de leeme.txt"
+  run "cat /home/vagrant/leeme.txt", on: :host
+  expect_one "IDP2223"
+
+end
+
+play do
+  show
+  export 
+  send copy_to: :host
+end
